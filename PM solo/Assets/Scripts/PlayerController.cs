fbff4fea7f48f6e3ff707c35b8269b70d323288b
Orlein52 <orlein523@gmail.com>
2025-09-11 12:45:05 -0700

    Camera control

--- PM solo/Assets/Scripts/PlayerController.cs
+++ PM solo/Assets/Scripts/PlayerController.cs
@@ -2,26 +2,46 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
     Rigidbody rb;
 
     float inputX;
     float inputY;
     public float speed = 5f;
+    public float Xsensitivity = 0.1f;
+    public float Ysensitivity = 0.1f;
+    public float cameraYMaxMin = 179;
 
+    Vector3 camerOffset = new Vector3 (0, 3.5f, -6);
+    Camera playerCam;
+    InputAction lookAxis;
+    Vector2 cameraRotation = new Vector2 (-10, 0);
+    
 
 
     void Start()
     {
         rb = GetComponent <Rigidbody>();
 
-
-
+        playerCam = Camera.main;
+        lookAxis = GetComponent<PlayerInput>().currentActionMap.FindAction("Look");
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
     
     // Update is called once per frame
     void Update()
     {
+
+        //Camera
+        playerCam.transform.position = transform.position + (camerOffset);
+        cameraRotation.x += lookAxis.ReadValue<Vector2>().x * Xsensitivity;
+        cameraRotation.y += lookAxis.ReadValue<Vector2>().y * Ysensitivity;
+        cameraRotation.y = Mathf.Clamp(cameraRotation.y, -cameraYMaxMin, cameraYMaxMin);
+        playerCam.transform.rotation = Quaternion.Euler(-cameraRotation.y, cameraRotation.x, 0);
+        transform.rotation = Quaternion.AngleAxis(cameraRotation.x, Vector3.up);
+
+        //Movement
         Vector3 tempMove = rb.linearVelocity;
         tempMove.x = inputY * speed;
         tempMove.z = inputX * speed;
